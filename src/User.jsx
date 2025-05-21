c69406493364b5b8f9a07996672bd61e7eae1978
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2025-05-21 16:51:32 +0100

    Update User.jsx

--- src/User.jsx
+++ src/User.jsx
@@ -4,7 +4,7 @@ function User() {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    fetch("http://tesa-alb-343047390.eu-west-1.elb.amazonaws.com/user")
+    fetch("http://3.252.69.33:5000/user")
       .then((res) => res.json())
       .then((json) => {
         console.log(json);
